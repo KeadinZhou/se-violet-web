ee29efb3c06c4b249523da8291eb42156c836cf2
KeadinZhou <31701030@stu.zucc.edu.cn>
2019-12-06 23:03:40 +0800

    index bigImg.

--- src/components/index-page/index-carousel.vue
+++ src/components/index-page/index-carousel.vue
@@ -3,7 +3,9 @@
         <div class="block">
             <el-carousel height="420px">
                 <el-carousel-item v-for="(item, index) in imgList" :key="index">
-                    <img :src="item" alt="">
+                    <a :href="item.target_url">
+                        <img :src="item.img_url" alt="">
+                    </a>
                 </el-carousel-item>
             </el-carousel>
         </div>
@@ -15,14 +17,40 @@
         name: "index-carousel",
         data () {
             return {
-                imgList: [
-                    'http://kealine.top/SE/bigimg/0',
-                    'http://kealine.top/SE/bigimg/1',
-                    'http://kealine.top/SE/bigimg/2',
-                    'http://kealine.top/SE/bigimg/3',
-                    'http://kealine.top/SE/bigimg/4'
-                ]
+                imgList: []
             }
+        },
+        methods: {
+            getData () {
+                const that = this
+                that.$http.post(that.$store.state.api + '/v1/ads/index_ads')
+                    .then(data => {
+                        const Data = data.data
+                        console.log(Data)
+                        if (Data.code === 0) {
+                            that.imgList = Data.data
+                        } else {
+                            const msg = Data.msg
+                            console.log(msg)
+                            if ((typeof msg) === 'string') {
+                                that.$message.error(msg)
+                            } else {
+                                for (const item in msg) {
+                                    that.$message.error(msg[item][0])
+                                }
+                            }
+                        }
+                    })
+                    .catch(function (error) {
+                        if (error.response) {
+                            console.log(error.response)
+                            that.$message.error('系统错误')
+                        }
+                    })
+            }
+        },
+        created () {
+            this.getData()
         }
     }
 </script>
